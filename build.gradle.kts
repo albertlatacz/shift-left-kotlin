860ce0d17a59ec8278f73b067521da08bb01ac92
Albert Latacz <albert.latacz@gmail.com>
2024-09-13 19:00:41 +0100

    moved app

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,9 @@
-import org.gradle.api.JavaVersion.VERSION_21
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
 import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 
 plugins {
     kotlin("jvm") version "2.0.20"
     id("shiftleft-root")
-    application
 }
 
 buildscript {
@@ -14,51 +11,6 @@ buildscript {
         mavenCentral()
         gradlePluginPortal()
     }
-}
-
-kotlin {
-    jvmToolchain {
-        languageVersion.set(JavaLanguageVersion.of(21))
-    }
-}
-
-application {
-    mainClass = "com.example.HelloWorldKt"
-}
-
-repositories {
-    mavenCentral()
-}
-
-tasks {
-    withType<KotlinJvmCompile>().configureEach {
-        compilerOptions {
-            allWarningsAsErrors = false
-            jvmTarget.set(JVM_11)
-            freeCompilerArgs.add("-Xjvm-default=all")
-        }
-    }
-
-    withType<Test> {
-        useJUnitPlatform()
-    }
-
-    java {
-        sourceCompatibility = VERSION_21
-        targetCompatibility = VERSION_21
-    }
-}
-
-dependencies {
-    implementation(platform("org.http4k:http4k-bom:5.29.0.0"))
-    implementation(platform("org.http4k:http4k-connect-bom:5.23.0.0"))
-    implementation("org.http4k:http4k-connect-amazon-s3")
-    implementation("org.http4k:http4k-core")
-    implementation("org.http4k:http4k-connect-google-analytics-ga4")
-    testImplementation("org.http4k:http4k-connect-amazon-s3-fake")
-    testImplementation("org.http4k:http4k-connect-google-analytics-ga4-fake")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.0")
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.11.0")
 }
 
 allprojects {
@@ -79,8 +31,14 @@ allprojects {
             compilerOptions {
                 jvmTarget = JvmTarget.JVM_21
                 allWarningsAsErrors = true
-                compilerOptions.freeCompilerArgs.add("-Xcontext-receivers")
-                compilerOptions.freeCompilerArgs.add("-opt-in=kotlin.contracts.ExperimentalContracts")
+            }
+        }
+
+        named<Test>("test").configure {
+            useJUnitPlatform {
+                reports {
+                    html.required = true
+                }
             }
         }
     }
--- module1/build.gradle.kts
+++ module1/build.gradle.kts
@@ -1,14 +1,20 @@
 import shiftleftkotlin.ShiftLeftModule
 import shiftleftkotlin.Team.TEAM1
 
-plugins {
-    id("shiftleft-module")
-}
+apply(plugin = "shiftleft-module")
 
 configure<ShiftLeftModule> {
     team.set(TEAM1)
 }
 
 dependencies {
-    implementation(project(":module2"))
+    implementation(platform("org.http4k:http4k-bom:5.29.0.0"))
+    implementation(platform("org.http4k:http4k-connect-bom:5.23.0.0"))
+    implementation("org.http4k:http4k-connect-amazon-s3")
+    implementation("org.http4k:http4k-core")
+    implementation("org.http4k:http4k-connect-google-analytics-ga4")
+    testImplementation("org.http4k:http4k-connect-amazon-s3-fake")
+    testImplementation("org.http4k:http4k-connect-google-analytics-ga4-fake")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.11.0")
 }
