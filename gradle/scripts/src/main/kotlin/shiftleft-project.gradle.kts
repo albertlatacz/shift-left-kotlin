178c472e449bda50857131f95d685acb6931f732
Albert Latacz <albert.latacz@gmail.com>
2024-09-15 14:19:50 +0100

    removed obsolite files

--- gradle/scripts/src/main/kotlin/shiftleft-project.gradle.kts
+++ gradle/scripts/src/main/kotlin/shiftleft-project.gradle.kts
@@ -1,10 +1,7 @@
-import shiftleftkotlin.generators.GenerateDependencyDiagram
 import shiftleftkotlin.generators.GenerateProjectReadme
 
-apply<GenerateDependencyDiagram>()
 apply<GenerateProjectReadme>()
 
 tasks {
-    named("check").get().finalizedBy(named("generateDependencyDiagram").get())
     named("check").get().finalizedBy(named("generateProjectReadme").get())
 }
