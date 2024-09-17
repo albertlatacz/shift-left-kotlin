ef9741c0b1fda9706ff9a7927e3667fec50e9bcb
Albert Latacz <albert.latacz@gmail.com>
2024-09-17 13:31:44 +0100

    import

--- libraries/core/src/test/kotlin/shiftleftkotlin/core/CoreArchTest.kt
+++ libraries/core/src/test/kotlin/shiftleftkotlin/core/CoreArchTest.kt
@@ -2,7 +2,6 @@ package shiftleftkotlin.core
 
 import com.tngtech.archunit.core.importer.ImportOption.DoNotIncludeTests
 import com.tngtech.archunit.junit.AnalyzeClasses
-import shiftleftkotlin.core.ArchitectureTestCase
 
 @AnalyzeClasses(packagesOf = [CoreArchTest::class], importOptions = [DoNotIncludeTests::class])
 object CoreArchTest : ArchitectureTestCase
