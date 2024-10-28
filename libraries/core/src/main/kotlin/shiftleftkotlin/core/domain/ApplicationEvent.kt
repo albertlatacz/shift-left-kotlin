86935670d1f78780687c9893d97f6a9a6a70ceb6
Albert Latacz <albert.latacz@gmail.com>
2024-10-28 06:22:09 +0100

    added type

--- libraries/core/src/main/kotlin/shiftleftkotlin/core/domain/ApplicationEvent.kt
+++ libraries/core/src/main/kotlin/shiftleftkotlin/core/domain/ApplicationEvent.kt
@@ -2,6 +2,6 @@ package shiftleftkotlin.core.domain
 
 import org.http4k.events.Event
 
-abstract class ApplicationEvent() : Event {
+abstract class ApplicationEvent : Event {
     val type: String = javaClass.simpleName
 }
